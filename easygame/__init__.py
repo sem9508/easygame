9ea6bfd66c0f2d478e5e08c061edb7a368b1546e
sem9508 <sem40911@gmail.com>
2025-02-22 20:03:50 +0100

    init message

--- easygame/__init__.py
+++ easygame/__init__.py
@@ -4,4 +4,4 @@ from . import utils
 def test_message():
     print('test succesful.')
 
-print('Hello from the easygame community.')
+print('Hello from the easygame community. https://github.com/sem9508/easygame/')
